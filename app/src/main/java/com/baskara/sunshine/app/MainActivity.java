f91f1b0232b311a82833d2acc1126c44a2307249
Baskara Patria <baskara.patria@hotmail.com>
2016-04-01 23:36:35 +0700

    [Baskara] init fake data using array string

--- app/src/main/java/com/baskara/sunshine/app/MainActivity.java
+++ app/src/main/java/com/baskara/sunshine/app/MainActivity.java
@@ -5,6 +5,16 @@ import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
+    private String[] fakeData = {
+            "Monday",
+            "Tuesday",
+            "Wednesday",
+            "Thursday",
+            "Friday",
+            "Saturday",
+            "Sunday",
+    };
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
