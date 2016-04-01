0a04c69862f5eafdd1b6e6f67081031e7d57a0f7
Baskara Patria <baskara.patria@hotmail.com>
2016-04-01 23:53:59 +0700

    [Baskara] Create fragment, move fakedata into fragment

---
+++ app/src/main/java/com/baskara/sunshine/app/ForecastFragment.java
@@ -0,0 +1,39 @@
+package com.baskara.sunshine.app;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class ForecastFragment extends Fragment {
+
+    private String[] fakeData = {
+            "Monday",
+            "Tuesday",
+            "Wednesday",
+            "Thursday",
+            "Friday",
+            "Saturday",
+            "Sunday",
+    };
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        super.onCreateOptionsMenu(menu, inflater);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return super.onCreateView(inflater, container, savedInstanceState);
+    }
+}
--- app/src/main/java/com/baskara/sunshine/app/MainActivity.java
+++ app/src/main/java/com/baskara/sunshine/app/MainActivity.java
@@ -5,16 +5,6 @@ import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
-    private String[] fakeData = {
-            "Monday",
-            "Tuesday",
-            "Wednesday",
-            "Thursday",
-            "Friday",
-            "Saturday",
-            "Sunday",
-    };
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
