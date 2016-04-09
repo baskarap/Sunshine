55b3a86060cf4e7a59afe6eb5596d76d99affbf2
Baskara Patria <baskara.patria@hotmail.com>
2016-04-10 02:40:27 +0700

    [Baskara] add share handler in DetailFragment

--- app/src/main/java/com/baskara/sunshine/app/DetailActivityFragment.java
+++ app/src/main/java/com/baskara/sunshine/app/DetailActivityFragment.java
@@ -3,6 +3,8 @@ package com.baskara.sunshine.app;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.view.MenuItemCompat;
+import android.support.v7.widget.ShareActionProvider;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
@@ -12,6 +14,10 @@ import android.view.ViewGroup;
 import android.widget.TextView;
 
 public class DetailActivityFragment extends Fragment {
+
+    private final String TAG = this.getClass().getSimpleName();
+    private static final String HASHTAG = "#SunshineApp";
+    private String forecast;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -25,7 +31,7 @@ public class DetailActivityFragment extends Fragment {
         View rootView = inflater.inflate(R.layout.fragment_detail, container, false);
         Intent intent = getActivity().getIntent();
         if (intent != null && intent.hasExtra(Intent.EXTRA_TEXT)) {
-            String forecast = intent.getStringExtra(Intent.EXTRA_TEXT);
+            forecast = intent.getStringExtra(Intent.EXTRA_TEXT);
             TextView textView = (TextView) rootView.findViewById(R.id.text_detail);
             textView.setText(forecast);
         }
@@ -35,6 +41,11 @@ public class DetailActivityFragment extends Fragment {
     @Override
     public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
         inflater.inflate(R.menu.menu_detail, menu);
+        MenuItem menuItem = menu.findItem(R.id.action_share);
+        ShareActionProvider shareActionProvider = (ShareActionProvider) MenuItemCompat.getActionProvider(menuItem);
+        if (forecast != null) {
+            shareActionProvider.setShareIntent(getShareIntent());
+        }
     }
 
     @Override
@@ -43,7 +54,16 @@ public class DetailActivityFragment extends Fragment {
         if (itemId == R.id.action_settings) {
             Intent intent = new Intent(getActivity(), SettingsActivity.class);
             startActivity(intent);
+            return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    private Intent getShareIntent() {
+        Intent shareIntent = new Intent(Intent.ACTION_SEND);
+        shareIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_WHEN_TASK_RESET);
+        shareIntent.setType("text/plain");
+        shareIntent.putExtra(Intent.EXTRA_TEXT, forecast + " " + HASHTAG);
+        return shareIntent;
     }
 }
