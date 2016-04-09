a09511b865c5c447e9d9dc1735387972aa2a759f
Baskara Patria <baskara.patria@hotmail.com>
2016-04-09 23:21:37 +0700

    [Baskara] add menu in DetailActivity, prepare SettingsActivity

--- app/src/main/java/com/baskara/sunshine/app/DetailActivityFragment.java
+++ app/src/main/java/com/baskara/sunshine/app/DetailActivityFragment.java
@@ -4,11 +4,20 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
 public class DetailActivityFragment extends Fragment {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setHasOptionsMenu(true);
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -21,5 +30,19 @@ public class DetailActivityFragment extends Fragment {
             textView.setText(forecast);
         }
         return rootView;
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        inflater.inflate(R.menu.menu_detail, menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int itemId = item.getItemId();
+        if (itemId == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
