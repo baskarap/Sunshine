999c47dc5b3ee0a1d9349ac23eb7d51c9d68c980
Baskara Patria <baskara.patria@hotmail.com>
2016-04-23 22:18:30 +0700

    [Baskara] prepare tables (location & weather)

--- app/src/main/java/com/baskara/sunshine/app/data/WeatherContract.java
+++ app/src/main/java/com/baskara/sunshine/app/data/WeatherContract.java
@@ -1,14 +1,23 @@
 package com.baskara.sunshine.app.data;
 
+import android.provider.BaseColumns;
 import android.text.format.Time;
 
 public class WeatherContract {
-    
+
     public static long normalizeDate(long startDate) {
         // normalize the start date to the beginning of the (UTC) day
         Time time = new Time();
         time.set(startDate);
         int julianDay = Time.getJulianDay(startDate, time.gmtoff);
         return time.setJulianDay(julianDay);
+    }
+
+    public static final class LocationEntry implements BaseColumns {
+        public static final String TABLE_NAME = "location";
+    }
+
+    public static final class WeatherEntry implements BaseColumns {
+        public static final String TABLE_NAME = "weather";
     }
 }
