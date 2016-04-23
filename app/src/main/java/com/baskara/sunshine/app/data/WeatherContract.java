b47412cff438dc9a4e11523e8c7550c168036b57
Baskara Patria <baskara.patria@hotmail.com>
2016-04-23 22:12:27 +0700

    [Baskara] add nomalizeDate method to normalize the start date to UTC

--- app/src/main/java/com/baskara/sunshine/app/data/WeatherContract.java
+++ app/src/main/java/com/baskara/sunshine/app/data/WeatherContract.java
@@ -1,4 +1,14 @@
 package com.baskara.sunshine.app.data;
 
+import android.text.format.Time;
+
 public class WeatherContract {
+    
+    public static long normalizeDate(long startDate) {
+        // normalize the start date to the beginning of the (UTC) day
+        Time time = new Time();
+        time.set(startDate);
+        int julianDay = Time.getJulianDay(startDate, time.gmtoff);
+        return time.setJulianDay(julianDay);
+    }
 }
