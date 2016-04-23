9a089fc923cf61aed1ac4bece4a4e7bdfeb74c31
Baskara Patria <baskara.patria@hotmail.com>
2016-04-23 23:19:27 +0700

    [Baskara] prepare db helper for weather

---
+++ app/src/main/java/com/baskara/sunshine/app/data/WeatherDbHelper.java
@@ -0,0 +1,26 @@
+package com.baskara.sunshine.app.data;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class WeatherDbHelper extends SQLiteOpenHelper {
+
+    private static final String DATABASE_NAME = "sunshine.db";
+    private static final int DATABASE_VERSION = 1;
+
+
+    public WeatherDbHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+}
