0df9615c3f18591946ffafd565d7e0556d7f46bd
Baskara Patria <baskara.patria@hotmail.com>
2016-04-23 23:23:54 +0700

    [Baskara] create table weather inside db helper

--- app/src/main/java/com/baskara/sunshine/app/data/WeatherDbHelper.java
+++ app/src/main/java/com/baskara/sunshine/app/data/WeatherDbHelper.java
@@ -4,6 +4,9 @@ import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
+import com.baskara.sunshine.app.data.WeatherContract.WeatherEntry;
+import com.baskara.sunshine.app.data.WeatherContract.LocationEntry;
+
 public class WeatherDbHelper extends SQLiteOpenHelper {
 
     private static final String DATABASE_NAME = "sunshine.db";
@@ -16,7 +19,27 @@ public class WeatherDbHelper extends SQLiteOpenHelper {
 
     @Override
     public void onCreate(SQLiteDatabase db) {
+        final String SQL_CREATE_WEATHER_TABLE = "CREATE TABLE " + WeatherEntry.TABLE_NAME + " (" +
+                WeatherEntry._ID + " INTEGER PRIMARY KEY AUTOINCREMENT," +
 
+                WeatherEntry.COLUMN_LOC_KEY + " INTEGER NOT NULL, " +
+                WeatherEntry.COLUMN_DATE + " INTEGER NOT NULL, " +
+                WeatherEntry.COLUMN_SHORT_DESCRIPTION + " TEXT NOT NULL, " +
+                WeatherEntry.COLUMN_WEATHER_ID + " INTEGER NOT NULL," +
+                WeatherEntry.COLUMN_MIN_TEMP + " REAL NOT NULL, " +
+                WeatherEntry.COLUMN_MAX_TEMP + " REAL NOT NULL, " +
+                WeatherEntry.COLUMN_HUMIDITY + " REAL NOT NULL, " +
+                WeatherEntry.COLUMN_PRESSURE + " REAL NOT NULL, " +
+                WeatherEntry.COLUMN_WIND_SPEED + " REAL NOT NULL, " +
+                WeatherEntry.COLUMN_DEGREES + " REAL NOT NULL, " +
+
+                " FOREIGN KEY (" + WeatherEntry.COLUMN_LOC_KEY + ") REFERENCES " +
+                LocationEntry.TABLE_NAME + " (" + LocationEntry._ID + "), " +
+
+                " UNIQUE (" + WeatherEntry.COLUMN_DATE + ", " +
+                WeatherEntry.COLUMN_LOC_KEY + ") ON CONFLICT REPLACE);";
+        
+        db.execSQL(SQL_CREATE_WEATHER_TABLE);
     }
 
     @Override
