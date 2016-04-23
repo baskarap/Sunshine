dcd93ffeb81f7907a35a0b4be8cc388cb2fcc0cc
Baskara Patria <baskara.patria@hotmail.com>
2016-04-23 23:29:22 +0700

    [Baskara] add SQL location table creation

--- app/src/main/java/com/baskara/sunshine/app/data/WeatherDbHelper.java
+++ app/src/main/java/com/baskara/sunshine/app/data/WeatherDbHelper.java
@@ -19,6 +19,14 @@ public class WeatherDbHelper extends SQLiteOpenHelper {
 
     @Override
     public void onCreate(SQLiteDatabase db) {
+        final String SQL_CREATE_LOCATION_TABLE = "CREATE TABLE " + LocationEntry.TABLE_NAME + " (" +
+                LocationEntry._ID + " INTEGER PRIMARY KEY," +
+                LocationEntry.COLUMN_LOCATION_SETTING + " TEXT UNIQUE NOT NULL, " +
+                LocationEntry.COLUMN_CITY_NAME + " TEXT NOT NULL, " +
+                LocationEntry.COLUMN_COORD_LAT + " REAL NOT NULL, " +
+                LocationEntry.COLUMN_COORD_LONG + " REAL NOT NULL " +
+                " );";
+
         final String SQL_CREATE_WEATHER_TABLE = "CREATE TABLE " + WeatherEntry.TABLE_NAME + " (" +
                 WeatherEntry._ID + " INTEGER PRIMARY KEY AUTOINCREMENT," +
 
@@ -38,7 +46,8 @@ public class WeatherDbHelper extends SQLiteOpenHelper {
 
                 " UNIQUE (" + WeatherEntry.COLUMN_DATE + ", " +
                 WeatherEntry.COLUMN_LOC_KEY + ") ON CONFLICT REPLACE);";
-        
+
+        db.execSQL(SQL_CREATE_LOCATION_TABLE);
         db.execSQL(SQL_CREATE_WEATHER_TABLE);
     }
 
